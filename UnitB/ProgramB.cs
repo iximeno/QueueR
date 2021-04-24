20607f146922bfc73bacd4846b4c4d34c2ac9b57
iximeno <58092638+iximeno@users.noreply.github.com>
2021-04-24 17:51:26 +0200

    Add files via upload

---
+++ UnitB/ProgramB.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitX;
+
+
+namespace UnitX
+{
+    class ProgramB
+    {
+        private static Queuer MyQueue = new Queuer();
+        public static List<Product> Products = new List<Product>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started !");
+            Products.Add(new Product(new Guid("cc69c7a0-72ca-4a48-9bbf-be4310ffe2a2")));          
+
+            MyQueue.ReadQueue("Solds", SoldFunction);
+            Console.ReadLine();
+            Console.WriteLine("Bye !");
+        }
+
+       
+        public static object SoldFunction(BasicDeliverEventArgs ea)
+        {
+            try
+            {
+                var body = ea.Body.ToArray();
+                var id = Encoding.UTF8.GetString(body);
+                var target = (from p in Products where p.Id.ToString("N") == id select p).FirstOrDefault();
+                if (target == null) return false;
+
+                target.Sold(DateTime.Now);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+        
+    }
+}
